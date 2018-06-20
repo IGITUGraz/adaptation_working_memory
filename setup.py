79fe9d137943a73cababadef9bd0d1af0891f1f1
Guillaume Bellec <guillweb@gmail.com>
2018-06-20 08:57:22 +0200

    update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import re
 from setuptools import setup, find_packages
 
 __author__ = "Guillaume Bellec, Darjan Salaj, Anand Subramoney"
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 
 
 def get_requirements(filename):
