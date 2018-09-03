3147fb1cf2d135664dc1d9f24f9ccde8dea425b5
Salaj Darjan <salaj@gmx.at>
2018-09-03 16:08:34 +0200

    Added XOR task

---
+++ bin/tutorial_temporalXOR_utils.py
@@ -0,0 +1,39 @@
+import numpy as np
+import numpy.random as rd
+
+
+def generate_xor_input(batch_size, length, pulse_delay=100, pulse_duration=30, out_duration=100):
+    input_nums = np.zeros((batch_size, length), dtype=float)
+    gocue_nums = np.zeros((batch_size, length), dtype=float)
+    targets = np.zeros((batch_size), dtype=int)
+    target_nums = np.zeros((batch_size, length), dtype=int)
+    target_mask_nums = np.zeros((batch_size, length), dtype=int)
+
+    def pulse_trace():
+        return np.sin(np.linspace(0, np.pi, pulse_duration))
+
+    for b in range(batch_size):
+        seq = np.zeros(length)
+        p1 = rd.choice([-1, 1])
+        p2 = rd.choice([-1, 1])
+        pulse_1 = pulse_trace() * p1
+        pulse_2 = pulse_trace() * p2
+        seq[0:pulse_duration] += pulse_1
+        seq[pulse_duration+pulse_delay:pulse_duration*2+pulse_delay] += pulse_2
+        input_nums[b, :] = seq
+
+        gocue_seq = np.zeros(length)
+        end_pulses = np.nonzero(seq)[0][-1]
+        gocue_seq[end_pulses+pulse_delay:end_pulses+pulse_delay+pulse_duration] += pulse_trace()
+        gocue_nums[b, :] = gocue_seq
+
+        # target = 1 if p1 != p2 else -1
+        target = p1 != p2
+        targets[b] = target
+        start_cue = end_pulses+pulse_delay
+        target_nums[b, start_cue:start_cue+pulse_duration+out_duration] = target
+
+        target_mask_nums[b, start_cue:start_cue+out_duration] = 1
+
+    network_input = np.stack((input_nums, gocue_nums), axis=-1)  # batch x length x 2
+    return network_input, target_nums, target_mask_nums, targets
--- bin/tutorial_temporalXOR_with_LSNN.py
+++ bin/tutorial_temporalXOR_with_LSNN.py
@@ -1,3 +1,4 @@
+from bin.tutorial_temporalXOR_utils import generate_xor_input
 from lsnn.guillaume_toolbox.tensorflow_einsums.einsum_re_written import einsum_bij_jk_to_bik
 # import matplotlib
 # matplotlib.use('Agg')
@@ -33,7 +34,7 @@ tf.app.flags.DEFINE_integer('batch_train', 128, 'batch size fo the validation se
 tf.app.flags.DEFINE_integer('batch_val', 128, 'batch size of the validation set')
 tf.app.flags.DEFINE_integer('batch_test', 128, 'batch size of the testing set')
 tf.app.flags.DEFINE_integer('n_charac', 2, 'number of characters in the recall task')
-tf.app.flags.DEFINE_integer('n_in', 100, 'number of input units.')
+tf.app.flags.DEFINE_integer('n_in', 2, 'number of input units.')
 tf.app.flags.DEFINE_integer('n_regular', 60, 'number of recurrent units.')
 tf.app.flags.DEFINE_integer('n_adaptive', 20, 'number of controller units')
 tf.app.flags.DEFINE_integer('f0', 50, 'input firing rate')
@@ -42,20 +43,20 @@ tf.app.flags.DEFINE_integer('reg_max_rate', 100, 'target rate for regularization
 tf.app.flags.DEFINE_integer('n_iter', 200, 'number of iterations')
 tf.app.flags.DEFINE_integer('n_delay', 10, 'number of delays')
 tf.app.flags.DEFINE_integer('n_ref', 3, 'Number of refractory steps')
-tf.app.flags.DEFINE_integer('seq_len', 12, 'Number of character steps')
-tf.app.flags.DEFINE_integer('seq_delay', 6, 'Expected delay in character steps. Must be <= seq_len - 2')
-tf.app.flags.DEFINE_integer('tau_char', 200, 'Duration of symbols')
+tf.app.flags.DEFINE_integer('seq_len', 8, 'Number of character steps')
+tf.app.flags.DEFINE_integer('seq_delay', 1, 'Expected delay in character steps. Must be <= seq_len - 2')
+tf.app.flags.DEFINE_integer('tau_char', 50, 'Duration of symbols')
 tf.app.flags.DEFINE_integer('seed', -1, 'Random seed.')
 tf.app.flags.DEFINE_integer('lr_decay_every', 100, 'Decay every')
 tf.app.flags.DEFINE_integer('print_every', 20, 'Decay every')
 ##
 tf.app.flags.DEFINE_float('stop_crit', 0.05, 'Stopping criterion. Stops training if error goes below this value')
 tf.app.flags.DEFINE_float('beta', 1.7, 'Mikolov adaptive threshold beta scaling parameter')
-tf.app.flags.DEFINE_float('tau_a', 1200, 'Mikolov model alpha - threshold decay')
+tf.app.flags.DEFINE_float('tau_a', 200, 'Mikolov model alpha - threshold decay')
 tf.app.flags.DEFINE_float('tau_out', 20, 'tau for PSP decay in LSNN and output neurons')
-tf.app.flags.DEFINE_float('learning_rate', 0.01, 'Base learning rate.')
+tf.app.flags.DEFINE_float('learning_rate', 0.005, 'Base learning rate.')
 tf.app.flags.DEFINE_float('lr_decay', 0.3, 'Decaying factor')
-tf.app.flags.DEFINE_float('reg', 1., 'regularization coefficient')
+tf.app.flags.DEFINE_float('reg', 1e-1, 'regularization coefficient')
 tf.app.flags.DEFINE_float('rewiring_connectivity', -1, 'possible usage of rewiring with ALIF and LIF (0.1 is default)')
 tf.app.flags.DEFINE_float('readout_rewiring_connectivity', -1, '')
 tf.app.flags.DEFINE_float('l1', 1e-2, 'l1 regularization that goes with rewiring')
@@ -145,38 +146,39 @@ file_reference = file_reference + '_taua' + str(FLAGS.tau_a) + '_beta' + str(FLA
 print('FILE REFERENCE: ' + file_reference)
 
 # Generate input
-input_spikes = tf.placeholder(dtype=tf.float32, shape=(None, None, FLAGS.n_in),
+input_spikes = tf.placeholder(dtype=tf.float32, shape=(None, None, 2),
                               name='InputSpikes')  # MAIN input spike placeholder
-input_nums = tf.placeholder(dtype=tf.int64, shape=(None, None),
-                            name='InputNums')  # Lists of input character for the recall task
 target_nums = tf.placeholder(dtype=tf.int64, shape=(None, None),
                              name='TargetNums')  # Lists of target characters of the recall task
 recall_mask = tf.placeholder(dtype=tf.bool, shape=(None, None),
                              name='RecallMask')  # Binary tensor that points to the time of presentation of a recall
 
 # Other placeholder that are useful for computing accuracy and debuggin
-target_sequence = tf.placeholder(dtype=tf.int64, shape=(None, None),
+target_sequence = tf.placeholder(dtype=tf.int64, shape=(None),
                                  name='TargetSequence')  # The target characters with time expansion
 batch_size_holder = tf.placeholder(dtype=tf.int32, name='BatchSize')  # Int that contains the batch size
 init_state_holder = placeholder_container_for_rnn_state(cell.state_size, dtype=tf.float32, batch_size=None)
-recall_charac_mask = tf.equal(input_nums, recall_symbol, name='RecallCharacMask')
+# recall_charac_mask = tf.equal(input_nums, recall_symbol, name='RecallCharacMask')
+recall_charac_mask = recall_mask
 
 
 def get_data_dict(batch_size, seq_len=FLAGS.seq_len, batch=None, override_input=None):
-    p_sr = 1/(1 + FLAGS.seq_delay)
-    spk_data, is_recall_data, target_seq_data, memory_seq_data, in_data, target_data = generate_storerecall_data(
-        batch_size=batch_size,
-        f0=input_f0,
-        sentence_length=seq_len,
-        n_character=FLAGS.n_charac,
-        n_charac_duration=FLAGS.tau_char,
-        n_neuron=FLAGS.n_in,
-        prob_signals=p_sr,
-        with_prob=True,
-        override_input=override_input,
-    )
-    data_dict = {input_spikes: spk_data, input_nums: in_data, target_nums: target_data, recall_mask: is_recall_data,
-                 target_sequence: target_seq_data, batch_size_holder: batch_size}
+    # p_sr = 1/(1 + FLAGS.seq_delay)
+    # spk_data, is_recall_data, target_seq_data, memory_seq_data, in_data, target_data = generate_storerecall_data(
+    #     batch_size=batch_size,
+    #     f0=input_f0,
+    #     sentence_length=seq_len,
+    #     n_character=FLAGS.n_charac,
+    #     n_charac_duration=FLAGS.tau_char,
+    #     n_neuron=FLAGS.n_in,
+    #     prob_signals=p_sr,
+    #     with_prob=True,
+    #     override_input=override_input,
+    # )
+    input_data, target_data, target_mask, targets = generate_xor_input(batch_size=batch_size,
+                                                                       length=FLAGS.seq_len * FLAGS.tau_char)
+    data_dict = {input_spikes: input_data, target_nums: target_data, recall_mask: target_mask,
+                 target_sequence: targets, batch_size_holder: batch_size}
 
     return data_dict
 
@@ -203,18 +205,24 @@ with tf.name_scope('RecallLoss'):
         w_out = tf.get_variable(name='out_weight', shape=[n_neurons, n_output_symbols])
 
     out = einsum_bij_jk_to_bik(psp, w_out)
-    out_char_step = tf_downsample(out, new_size=FLAGS.seq_len, axis=1)
-    Y_predict = tf.boolean_mask(out_char_step, recall_charac_mask, name='Prediction')
+    # out_char_step = tf_downsample(out, new_size=FLAGS.seq_len, axis=1)
+    # Y_predict = tf.boolean_mask(out_char_step, recall_charac_mask, name='Prediction')
+    Y_predict = tf.boolean_mask(out, recall_charac_mask, name='Prediction')
+    # Y_predict = tf.reduce_mean(Y_predict, axis=1)
+    # Y = tf.reduce_mean(target_nums_at_recall, axis=1)
 
-    # loss_recall = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=Y_predict))
     loss_recall = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=target_nums_at_recall,
                                                                                 logits=Y_predict))
 
     with tf.name_scope('PlotNodes'):
         out_plot = tf.nn.softmax(out)
-        out_plot_char_step = tf_downsample(out_plot, new_size=FLAGS.seq_len, axis=1)
+        # out_plot_char_step = tf_downsample(out_plot, new_size=FLAGS.seq_len, axis=1)
 
-    _, recall_errors, false_sentence_id_list = error_rate(out_char_step, target_nums, input_nums, n_charac)
+    # _, recall_errors, false_sentence_id_list = error_rate(out_char_step, target_nums, input_nums, n_charac)
+    Y_predict_hard = tf.argmax(Y_predict, axis=1)
+    all_accuracy = tf.cast(tf.equal(Y_predict_hard, tf.argmax(Y, axis=1)), dtype=tf.float32)
+    recall_accuracy = tf.reduce_mean(all_accuracy)
+    recall_errors = 1. - recall_accuracy
 
 # Target regularization
 with tf.name_scope('RegularizationLoss'):
@@ -299,56 +307,50 @@ def update_plot(plot_result_values, batch=0, n_max_neuron_per_raster=20, n_max_s
 
         if np.size(data) > 0:
             data = data[batch]
-            n_max = min(data.shape[1], n_max_neuron_per_raster)
-            cell_select = np.linspace(start=0, stop=data.shape[1] - 1, num=n_max, dtype=int)
-            data = data[:, cell_select]  # select a maximum of n_max_neuron_per_raster neurons to plot
-            raster_plot(ax, data, linewidth=0.3)
+            # n_max = min(data.shape[1], n_max_neuron_per_raster)
+            # cell_select = np.linspace(start=0, stop=data.shape[1] - 1, num=n_max, dtype=int)
+            # data = data[:, cell_select]  # select a maximum of n_max_neuron_per_raster neurons to plot
+            if k_data == 0:
+                presentation_steps = np.arange(data.shape[0])
+                ax.plot(presentation_steps, data[:, 0], color='blue', label='Input', alpha=0.7)
+                ax.plot(presentation_steps, data[:, 1], color='red', label='Go-cue', alpha=0.7)
+                ax.axis([0, len(data), -1, 1])
+            else:
+                raster_plot(ax, data, linewidth=0.3)
             ax.set_ylabel(d_name)
             ax.set_xticklabels([])
 
-            if d_name == 'Input':
-                ax.set_yticklabels([])
-                n_channel = data.shape[1] // n_input_symbols
-                ax.add_patch(  # Value 0 row
-                    patches.Rectangle((0, 0), data.shape[0], n_channel, facecolor="red", alpha=0.15))
-                ax.add_patch(  # Value 1 row
-                    patches.Rectangle((0, n_channel), data.shape[0], n_channel, facecolor="blue", alpha=0.15))
-                ax.add_patch(  # Store row
-                    patches.Rectangle((0, 2 * n_channel), data.shape[0], n_channel, facecolor="yellow", alpha=0.15))
-                ax.add_patch(  # Recall row
-                    patches.Rectangle((0, 3 * n_channel), data.shape[0], n_channel, facecolor="green", alpha=0.15))
-
-                top_margin = 0.08
-                left_margin = -0.6
-                ax.text(left_margin, 1. - top_margin, 'Recall', transform=ax.transAxes, fontsize=7, verticalalignment='top')
-                ax.text(left_margin, 0.75 - top_margin, 'Store', transform=ax.transAxes, fontsize=7, verticalalignment='top')
-                ax.text(left_margin, 0.5 - top_margin, 'Value 1', transform=ax.transAxes, fontsize=7, verticalalignment='top')
-                ax.text(left_margin, 0.25 - top_margin, 'Value 0', transform=ax.transAxes, fontsize=7, verticalalignment='top')
 
     # plot targets
     ax = ax_list[2]
     mask = plot_result_values['recall_charac_mask'][batch]
     data = plot_result_values['target_nums'][batch]
-    data[np.invert(mask)] = -1
-    lines = []
-    ind_nt = np.argwhere(data != -1)
-    for idx in ind_nt.tolist():
-        i = idx[0]
-        lines.append([(i * FLAGS.tau_char, data[i]), ((i + 1) * FLAGS.tau_char, data[i])])
-    lc_t = mc.LineCollection(lines, colors='green', linewidths=2, label='Target')
-    ax.add_collection(lc_t)  # plot target segments
+    presentation_steps = np.arange(data.shape[0])
+    line_target, = ax.plot(presentation_steps[mask], data[mask], color='green', label='Target', alpha=0.7)
+    data = plot_result_values['out_plot'][batch, :, 1]
+    out_avg = data[mask]
+    out_avg = np.ones_like(out_avg) * np.mean(out_avg)
+    line_out, = ax.plot(presentation_steps[mask], out_avg, color='blue', label='avg.out.', alpha=0.7)
+    # data[np.invert(mask)] = -1
+    # lines = []
+    # ind_nt = np.argwhere(data != -1)
+    # for idx in ind_nt.tolist():
+    #     i = idx[0]
+    #     lines.append([(i * FLAGS.tau_char, data[i]), ((i + 1) * FLAGS.tau_char, data[i])])
+    # lc_t = mc.LineCollection(lines, colors='green', linewidths=2, label='Target')
+    # ax.add_collection(lc_t)  # plot target segments
 
-    # plot output per tau_char
-    data = plot_result_values['out_plot_char_step'][batch]
-    data = np.array([(d[1] - d[0] + 1) / 2 for d in data])
-    data[np.invert(mask)] = -1
-    lines = []
-    ind_nt = np.argwhere(data != -1)
-    for idx in ind_nt.tolist():
-        i = idx[0]
-        lines.append([(i * FLAGS.tau_char, data[i]), ((i + 1) * FLAGS.tau_char, data[i])])
-    lc_o = mc.LineCollection(lines, colors='blue', linewidths=2, label='Output')
-    ax.add_collection(lc_o)  # plot target segments
+    # # plot output per tau_char
+    # data = plot_result_values['out_plot_char_step'][batch]
+    # data = np.array([(d[1] - d[0] + 1) / 2 for d in data])
+    # data[np.invert(mask)] = -1
+    # lines = []
+    # ind_nt = np.argwhere(data != -1)
+    # for idx in ind_nt.tolist():
+    #     i = idx[0]
+    #     lines.append([(i * FLAGS.tau_char, data[i]), ((i + 1) * FLAGS.tau_char, data[i])])
+    # lc_o = mc.LineCollection(lines, colors='blue', linewidths=2, label='Output')
+    # ax.add_collection(lc_o)  # plot target segments
 
     # plot softmax of psp-s per dt for more intuitive monitoring
     # ploting only for second class since this is more intuitive to follow (first class is just a mirror)
@@ -357,9 +359,9 @@ def update_plot(plot_result_values, batch=0, n_max_neuron_per_raster=20, n_max_s
     ax.set_yticks([0, 0.5, 1])
     ax.grid(color='black', alpha=0.15, linewidth=0.4)
     ax.set_ylabel('Output')
-    line_output2, = ax.plot(presentation_steps, output2, color='purple', label='softmax', alpha=0.7)
+    line_output2, = ax.plot(presentation_steps, output2, color='purple', label='Output', alpha=0.7)
     ax.axis([0, presentation_steps[-1] + 1, -0.3, 1.1])
-    ax.legend(handles=[lc_t, lc_o, line_output2], loc='lower center', fontsize=7,
+    ax.legend(handles=[line_output2, line_target, line_out], loc='lower center', fontsize=7,
               bbox_to_anchor=(0.5, -0.05), ncol=3)
     ax.set_xticklabels([])
 
@@ -392,14 +394,15 @@ validation_error_list = []
 tau_delay_list = []
 training_time_list = []
 time_to_ref_list = []
-results_tensors = {'loss': loss,
-                   'loss_reg': loss_reg,
-                   'loss_recall': loss_recall,
-                   'recall_errors': recall_errors,
-                   'final_state': final_state,
-                   'av': av,
-                   'adaptive_regularization_coeff': adaptive_regularization_coeff,
-                   }
+results_tensors = {
+    'loss': loss,
+    'loss_reg': loss_reg,
+    'loss_recall': loss_recall,
+    'recall_errors': recall_errors,
+    'final_state': final_state,
+    'av': av,
+    'adaptive_regularization_coeff': adaptive_regularization_coeff,
+}
 
 results_tensors['w_in_val'] = cell.w_in_val
 results_tensors['w_rec_val'] = cell.w_rec_val
@@ -412,9 +415,8 @@ w_out_last = sess.run(w_out)
 plot_result_tensors = {'input_spikes': input_spikes,
                        'z': z,
                        'z_con': z_con,
-                       'input_nums': input_nums,
                        'target_nums': target_nums,
-                       'out_avg_per_step': out_plot_char_step,
+                       # 'out_avg_per_step': out_plot_char_step,
                        }
 t_train = 0
 t_ref = time()
@@ -431,7 +433,7 @@ for k_iter in range(FLAGS.n_iter):
     feed_dict_with_placeholder_container(val_dict, init_state_holder, last_final_state_state_validation_pointer[0])
 
     plot_result_tensors['psp'] = psp
-    plot_result_tensors['out_plot_char_step'] = out_plot_char_step
+    # plot_result_tensors['out_plot_char_step'] = out_plot_char_step
     plot_result_tensors['out_plot'] = out_plot
     plot_result_tensors['recall_charac_mask'] = recall_charac_mask
     plot_result_tensors['Y'] = Y
@@ -593,8 +595,8 @@ if FLAGS.save_data:
         feed_dict_with_placeholder_container(test_dict, init_state_holder, sess.run(
             cell.zero_state(batch_size=FLAGS.batch_train, dtype=tf.float32)))
 
-        results_values, plot_results_values, in_spk, spk, spk_con, target_nums_np, z_sum_np = sess.run(
-            [results_tensors, plot_result_tensors, input_spikes, z, z_con, target_nums, out_plot_char_step],
+        results_values, plot_results_values, in_spk, spk, spk_con, target_nums_np = sess.run(
+            [results_tensors, plot_result_tensors, input_spikes, z, z_con, target_nums],
             feed_dict=test_dict)
         # last_final_state_state_testing_pointer[0] = results_values['final_state']
         test_errors.append(results_values['recall_errors'])
